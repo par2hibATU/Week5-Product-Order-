fe841aee38430436a94d3ce0f00ad93018e69b13
par2hibATU <parthibbdcox@gmail.com>
2024-10-17 00:52:22 +0100

    Update method added and working

--- src/main/java/ie/atu/week5/customerapp/CustomerService.java
+++ src/main/java/ie/atu/week5/customerapp/CustomerService.java
@@ -10,6 +10,7 @@ import java.util.Optional;
 public class CustomerService {
     private final CustomerRepository customerRepository;
 
+
     @Autowired
     public CustomerService(CustomerRepository customerRepository) {
         this.customerRepository = customerRepository;
@@ -23,6 +24,16 @@ public class CustomerService {
     }
     public Customer createCustomer(Customer customer) {
         return customerRepository.save(customer);
+    }
+    public Customer updateCustomer(String id, Customer customer) {
+        Optional<Customer> customerOptional = customerRepository.findById(id);
+        if (!customerOptional.isPresent()) {
+            throw new RuntimeException("Customer not found");
+        }
+        Customer existingCustomer = customerOptional.get();
+        existingCustomer.setName(customer.getName());
+        existingCustomer.setEmail(customer.getEmail());
+        return customerRepository.save(existingCustomer);
     }
     public boolean deleteCustomer(String id) {
         if (customerRepository.existsById(id)) {
